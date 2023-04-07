21e97bb3e6f5e0e8c4f83945d5530bbfe4d2c428
hgoldman <honey.goldman.mahalla@intel.com>
2023-04-07 16:29:29 +0300

    added live search filters, by company & model.

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -14,6 +14,7 @@ export class SearchComponent implements OnInit {
   close = new EventEmitter<string>();
   searchText = ''
   company2Cars: Map<string, Car[]> = new Map();
+  shownData: Map<string, Car[]> = new Map();
   tesla: Car = new Car()
   ionic: Car = new Car()
   showFilters = false;
@@ -22,17 +23,17 @@ export class SearchComponent implements OnInit {
 
   ngOnInit(): void {
     this.tesla.logo = 'assets/Branding/tesla.png'
-    this.tesla.Model = 'Dual Motor'
+    this.tesla.Model = '3 Dual Motor'
     this.tesla.year = 2023
-    this.tesla.Company = 'טסלה מודל 3'
+    this.tesla.Company = 'Tesla'
     this.tesla.BatteryRange = '320 ק"מ'
     this.tesla.OrderLink = 'https://www.tesla.com/model3'
     this.tesla.Price = '320,000 ILS'
     this.tesla.ImagePath = 'assets/cars/tesla_m s 23.png'
     this.ionic.logo = 'assets/Branding/hyundai.gif'
-    this.ionic.Model = 'Sport Edition'
+    this.ionic.Model = 'EV6 Sport Edition'
     this.ionic.year = 2023
-    this.ionic.Company = 'קיה EV6'
+    this.ionic.Company = 'Hyundai'
     this.tesla.Rank = '4.5'
     this.ionic.ImagePath = 'assets/cars/ioniq 5 23.jpg'
     this.company2Cars.set('Hyundai', [this.ionic, this.ionic, this.ionic, this.ionic])
@@ -47,6 +48,7 @@ export class SearchComponent implements OnInit {
         this.comparingCarArr.push(car)
       }
     })
+    this.shownData = this.company2Cars
   }
 
 
@@ -70,6 +72,30 @@ export class SearchComponent implements OnInit {
 
   compare(){
     console.log('comparing!')
+  }
+
+  removeCar(index:number){
+      this.comparingCarArr.splice(index,1)
+      console.log(this.comparingCarArr)
+  }
+
+  filter(){
+    let results: Map<string, Car[]> = new Map();
+    this.company2Cars.forEach((cars:Car[],company:string) => {
+      let carArray:Car[]=[]
+        cars.forEach(car=>{
+          if(car.Company?.toLocaleLowerCase().includes(this.searchText.toLocaleLowerCase()) ||
+          car.Model?.toLocaleLowerCase().includes(this.searchText.toLocaleLowerCase()) ||
+          String(car.year).toLocaleLowerCase().includes(this.searchText.toLocaleLowerCase())
+          ){
+            carArray.push(car)
+          }
+          if(carArray.length !==0){
+            results.set(company,carArray);
+          }
+        });
+    });
+    this.shownData = results
   }
 
 }
